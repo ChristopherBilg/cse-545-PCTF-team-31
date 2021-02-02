6d65ccb3ddb4596bd04f952d216f89b801dec46d
Christopher Bilger <christopherbilg@gmail.com>
2021-02-02 16:32:53 -0500

    Initial module created for the quiet port scanner

---
+++ modules/quiet-scan.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def module_name():
+  return "quiet-scan"
+
+def module_help():
+  return "quiet port scanner tool"
+
+def module_usage():
+  return "{0}".format(module_name())
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
