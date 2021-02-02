2db49f3269213072dc4fed512b89091facf2c87a
Christopher Bilger <christopherbilg@gmail.com>
2021-02-02 16:28:46 -0500

    Initial module created for the network traffic analyzer

---
+++ modules/nta.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def module_name():
+  return "nta"
+
+def module_help():
+  return "network traffic analyzer tool"
+
+def module_usage():
+  return "{0}".format(module_name())
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
